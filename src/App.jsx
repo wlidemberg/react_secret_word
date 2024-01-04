37dc4d8086d63ba12d1b2ef1713db1f718d603f4
Berg Sousa <automatizei@gmail.com>
2024-01-04 18:02:20 -0300

    Divisão da tela principal do projeto

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
 import './App.css'
+import interrogacao from './assets/imagem_mulher_interrogacao.png'
 
 function App() {
   return (
     <>
-     
+     <div className='main_screen'>
+        <div className='screen_left'>
+          <img src={interrogacao} alt="Mulher com cartaz de interrogação" />
+        </div>
+        <div className='screen_right'>
+          <header>
+          <h1>Secret Word</h1>
+        </header>
+        </div>
+     </div>
     </>
   )
 }
